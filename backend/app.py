9bc9cf98554697d44bc24f1f0c00273a1781d69d
Adam Osgood <adamlee.osgood@gmail.com>
2020-12-03 12:03:30 +0100

    first push

--- backend/app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-# ! Hello world flask app to start you off.
-@app.route('/')
-def index():
-    return "Hello, World!"
---
+++ backend/seeds.py
@@ -0,0 +1,40 @@
+from app import app, db
+from models.user import User
+from models.contact import Contact
+
+with app.app_context():
+  db.drop_all()
+  db.create_all()
+
+  adam = User(
+    username='adam',
+    email='adam@gmail.com',
+    password='pass'
+  )
+
+  florian = User(
+    username='florian',
+    email='florian@gmail.com',
+    password='pass'
+  )
+
+  adam.save()
+  florian.save()
+  print('starting users created')
+  friend1 = Contact(
+    name='Jon',
+    birthday='02/03/2001',
+    user=adam
+  )
+  friend2 = Contact(
+    name='Bob',
+    birthday='02/01/2010',
+    user=florian
+  )
+  db.session.add(friend1)
+  db.session.add(friend2)
+  db.session.commit()
+  print('we have friends')
+
+
+  
