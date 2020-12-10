d9fdf7a0cbe593a9913c0b3557641205e0290760
Adam Osgood <adamlee.osgood@gmail.com>
2020-12-10 17:05:06 +0100

    heroku_hopefull_fix

--- backend/controllers/goog_auth.py
+++ backend/controllers/goog_auth.py
@@ -228,7 +228,7 @@ if __name__ == '__main__':
   # When running locally, disable OAuthlib's HTTPs verification.
   # ACTION ITEM for developers:
   #     When running in production *do not* leave this option enabled.
-  os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+  os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '0'
 
   # Specify a hostname and port that are set as a valid redirect URI
   # for your API project in the Google API Console.
